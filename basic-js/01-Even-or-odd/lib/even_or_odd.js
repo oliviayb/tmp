8a5fe22e47976ab7f66dea4690eca8e8f7bae171
Olivia Budd <buddolivia@gmail.com>
2020-07-07 21:30:38 +0800

    JS challenge evenOrOdd fuction

---
+++ basic-js/01-Even-or-odd/lib/even_or_odd.js
@@ -0,0 +1,10 @@
+evenOrOdd = (number) => {
+if (number % 2 == 0) {
+return "even";
+} else {
+return "odd";// TODO: this should return "even" if the number is even, "odd" otherwise
+}}
+
+console.log(evenOrOdd(3))
+
+
