d4beaa66005833098ff2eeb9ef58e24ce0d03b03
Olivia Budd <buddolivia@gmail.com>
2020-07-07 22:05:33 +0800

    challenge for sum of negative fuction

--- basic-js/02-Sum-of-negative/lib/sum_of_negative.js
+++ basic-js/02-Sum-of-negative/lib/sum_of_negative.js
@@ -1,3 +1,10 @@
 sumOfNegative = (numbers) => {
-  // TODO: You are getting a `numbers` array. Return the sum of **negative** numbers only.
+  let result = 0;
+  numbers.forEach((number) => {
+    if (number < 0) {
+      result += number;
+    }
+  })
+  return result // TODO: You are getting a `numbers` array. Return the sum of **negative** numbers only.
 }
+sumOfNegative([-1, 4, -2, 9, 18])
