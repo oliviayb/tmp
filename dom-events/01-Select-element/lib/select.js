fd93043b350ccfeccc41346078ebddad0aed2df2
Olivia Budd <buddolivia@gmail.com>
2020-07-09 22:00:00 +0800

    select an element challenge code cracked

---
+++ dom-events/01-Select-element/lib/select.js
@@ -0,0 +1,18 @@
+// This will check if you're able to run javascript.
+// If you don't see a popup greeting you, talk to a teacher!
+
+alert("Hello! You are running Javascript")
+
+const countries = document.querySelectorAll("#winner");
+countries.forEach((item) => {
+  console.log(item.innerText);
+});
+
+
+// TODO 1. Open the `index.html` file and set an id attribute to the France <li>
+// (nothing to write in this file for this step, write it in the html file)
+
+
+// TODO 2: select the France list item and return it!
+
+
